8c3ee9144d86d5c40a1a48b7c54ae0465781cd05
Alex <137922523+AlexKedrov@users.noreply.github.com>
2023-07-10 19:11:26 +0200

    05.07_HW-Java_Basic

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main {
+        public static void main(String...args) {
+            // Задание 2
+            int number1 = 27;
+            int number2 = 34;
+            int number3 = 48;
+
+            String auto1 = "BMW";
+            String auto2 = "AUDI";
+            String auto3 = "MERCEDES";
+
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(auto1);
+        System.out.println(auto2);
+        System.out.println(auto3);
+// Задание 3
+            int x = 50;
+            int y = 10;
+            int sum = x + y;
+            int sub = x - y;
+            int div = x / y;
+            int mult = x * y;
+            System.out.println(x);
+            System.out.println(y);
+            System.out.println(sum);
+            System.out.println(sub);
+            System.out.println(div);
+            System.out.println(mult);
+
+    }
+}
