215a0cc5873d0e75dcc30ee43e80ee8ff3a2d663
Alex <137922523+AlexKedrov@users.noreply.github.com>
2023-07-11 01:50:29 +0200

    Weather_Random_HW_TelRan

--- Main.java
+++ Main.java
@@ -1,34 +1,183 @@
+import com.daysoftheweek.DaysoftheWeek;
+import com.temrature.Temp;
+import com.weatherconditions.WeatherConditions;
+
 public class Main {
-        public static void main(String...args) {
-            // Задание 2
-            int number1 = 27;
-            int number2 = 34;
-            int number3 = 48;
+    public static void main(String[] args) {
+DaysoftheWeek Days = new DaysoftheWeek();
+WeatherConditions Conditions = new WeatherConditions();
+        Temp Temprature = new Temp();
 
-            String auto1 = "BMW";
-            String auto2 = "AUDI";
-            String auto3 = "MERCEDES";
+        int r1 = (int) (Math.random() * Temprature.HotWeather);
 
+// Понедельник
 
-        System.out.println(number1);
-        System.out.println(number2);
-        System.out.println(number3);
-        System.out.println(auto1);
-        System.out.println(auto2);
-        System.out.println(auto3);
-// Задание 3
-            int x = 50;
-            int y = 10;
-            int sum = x + y;
-            int sub = x - y;
-            int div = x / y;
-            int mult = x * y;
-            System.out.println(x);
-            System.out.println(y);
-            System.out.println(sum);
-            System.out.println(sub);
-            System.out.println(div);
-            System.out.println(mult);
+            System.out.println(Days.Monday);
+            System.out.println(r1 + "°C");
+            if (r1 == 0) {
+                System.out.println(Conditions.Cold);
+                System.out.println(Conditions.SnowWeather);
+            } else if (r1 > 0 && r1 < Temprature.ColdWeather) {
+                System.out.println(Conditions.Cold);
+            } else if (r1 >= Temprature.ColdWeather && r1 < Temprature.CoolWeather) {
+                System.out.println(Conditions.cool);
+            } else if (r1 >= Temprature.CoolWeather && r1 < Temprature.GoodWeather) {
+                System.out.println(Conditions.warm);
+            } else if (r1 >= Temprature.GoodWeather && r1 <= Temprature.HotWeather) {
+                System.out.println(Conditions.hot);
+            }
+
+            System.out.println(Conditions.WeatherCondition[Conditions.cond1]);
+            System.out.println(" ");
+
+        //Вторник
+
+        int r2 = (int) (Math.random() * Temprature.HotWeather);
+
+        System.out.println(Days.Tuesday);
+        System.out.println(r2 + "°C");
+        if (r2 == 0){
+            System.out.println(Conditions.Cold);
+            System.out.println(Conditions.SnowWeather);
+        }
+        else if (r2 > 0 && r2 < Temprature.ColdWeather) {
+            System.out.println(Conditions.Cold);
+        }
+        else if (r2 >= Temprature.ColdWeather && r2 < Temprature.CoolWeather) {
+            System.out.println(Conditions.cool);
+        }
+        else if (r2 >= Temprature.CoolWeather && r2 < Temprature.GoodWeather) {
+            System.out.println(Conditions.warm);
+        }
+        else if (r2 >= Temprature.GoodWeather && r2 <= Temprature.HotWeather) {
+            System.out.println(Conditions.hot);
+        }
+
+        System.out.println(Conditions.WeatherCondition[Conditions.cond2]);
+        System.out.println(" ");
+
+        //Среда
+        int r3 = (int) (Math.random() * Temprature.HotWeather);
+
+        System.out.println(Days.Wednsday);
+        System.out.println(r3 + "°C");
+        if (r3 == 0){
+            System.out.println(Conditions.Cold);
+            System.out.println(Conditions.SnowWeather);
+        }
+        else if (r3 > 0 && r3 < Temprature.ColdWeather) {
+            System.out.println(Conditions.Cold);
+        }
+        else if (r3 >= Temprature.ColdWeather && r3 < Temprature.CoolWeather) {
+            System.out.println(Conditions.cool);
+        }
+        else if (r3 >= Temprature.CoolWeather && r3 < Temprature.GoodWeather) {
+            System.out.println(Conditions.warm);
+        }
+        else if (r3 >= Temprature.GoodWeather && r3 <= Temprature.HotWeather) {
+            System.out.println(Conditions.hot);
+        }
+
+        System.out.println(Conditions.WeatherCondition[Conditions.cond3]);
+        System.out.println(" ");
+
+        //Четверг
+        int r4 = (int) (Math.random() * Temprature.HotWeather);
+
+        System.out.println(Days.Thursday);
+        System.out.println(r4 + "°C");
+        if (r4 == 0){
+            System.out.println(Conditions.Cold);
+            System.out.println(Conditions.SnowWeather);
+        }
+        else if (r4 > 0 && r4 < Temprature.ColdWeather) {
+            System.out.println(Conditions.Cold);
+        }
+        else if (r4 >= Temprature.ColdWeather && r4 < Temprature.CoolWeather) {
+            System.out.println(Conditions.cool);
+        }
+        else if (r4 >= Temprature.CoolWeather && r4 < Temprature.GoodWeather) {
+            System.out.println(Conditions.warm);
+        }
+        else if (r4 >= Temprature.GoodWeather && r4 <= Temprature.HotWeather) {
+            System.out.println(Conditions.hot);
+        }
+
+        System.out.println(Conditions.WeatherCondition[Conditions.cond4]);
+        System.out.println(" ");
+
+        //Пятница
+        int r5 = (int) (Math.random() * Temprature.HotWeather);
+
+        System.out.println(Days.Friday);
+        System.out.println(r5 + "°C");
+        if (r5 == 0){
+            System.out.println(Conditions.Cold);
+            System.out.println(Conditions.SnowWeather);
+        }
+        else if (r5 > 0 && r5 < Temprature.ColdWeather) {
+            System.out.println(Conditions.Cold);
+        }
+        else if (r5 >= Temprature.ColdWeather && r5 < Temprature.CoolWeather) {
+            System.out.println(Conditions.cool);
+        }
+        else if (r5 >= Temprature.CoolWeather && r5 < Temprature.GoodWeather) {
+            System.out.println(Conditions.warm);
+        }
+        else if (r5 >= Temprature.GoodWeather && r5 <= Temprature.HotWeather) {
+            System.out.println(Conditions.hot);
+        }
+
+        System.out.println(Conditions.WeatherCondition[Conditions.cond5]);
+        System.out.println(" ");
+
+        //Суббота
+        int r6 = (int) (Math.random() * Temprature.HotWeather);
+        System.out.println(Days.Saturday);
+        System.out.println(r6 + "°C");
+        if (r6 == 0){
+            System.out.println(Conditions.Cold);
+            System.out.println(Conditions.SnowWeather);
+        }
+        else if (r6 > 0 && r6 < Temprature.ColdWeather) {
+            System.out.println(Conditions.Cold);
+        }
+        else if (r6 >= Temprature.ColdWeather && r6 < Temprature.CoolWeather) {
+            System.out.println(Conditions.cool);
+        }
+        else if (r6 >= Temprature.CoolWeather && r6 < Temprature.GoodWeather) {
+            System.out.println(Conditions.warm);
+        }
+        else if (r6 >= Temprature.GoodWeather && r6 <= Temprature.HotWeather) {
+            System.out.println(Conditions.hot);
+        }
+
+        System.out.println(Conditions.WeatherCondition[Conditions.cond6]);
+        System.out.println(" ");
+
+        //Воскресенье
+        int r7 = (int) (Math.random() * Temprature.HotWeather);
+        System.out.println(Days.Sunday);
+        System.out.println(r7 + "°C");
+        if (r7 == 0){
+            System.out.println(Conditions.Cold);
+            System.out.println(Conditions.SnowWeather);
+        }
+        else if (r7 > 0 && r7 < Temprature.ColdWeather) {
+            System.out.println(Conditions.Cold);
+        }
+        else if (r7 >= Temprature.ColdWeather && r7 < Temprature.CoolWeather) {
+            System.out.println(Conditions.cool);
+        }
+        else if (r7 >= Temprature.CoolWeather && r7 < Temprature.GoodWeather) {
+            System.out.println(Conditions.warm);
+        }
+        else if (r7 >= Temprature.GoodWeather && r7 <= Temprature.HotWeather) {
+            System.out.println(Conditions.hot);
+        }
+
+        System.out.println(Conditions.WeatherCondition[Conditions.cond7]);
+    }
+
 
     }
-}
