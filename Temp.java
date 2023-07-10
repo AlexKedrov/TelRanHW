5f89473feadbfbd398d4551678968b41bab348aa
Alex <137922523+AlexKedrov@users.noreply.github.com>
2023-07-11 01:58:34 +0200

    Weather_TempClass_HW_TelRan

---
+++ Temp.java
@@ -0,0 +1,10 @@
+package com.temrature;
+
+public class Temp {
+    public byte HotWeather = 35;
+    public byte GoodWeather = 26;
+    public byte CoolWeather = 19;
+    public byte ColdWeather = 12;
+    public byte min = 5;
+
+}
